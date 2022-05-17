305967e04b68c81489a63949a6e80a6fe6681922
Leandro Matheus <kesuhige@gmail.com>
2022-05-17 19:32:04 -0300

    Add not found page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex min-h-full">
+        <div class="m-auto">
+            <img class="m-auto" src="https://lh3.googleusercontent.com/fife/AAWUweW3O6O2Nl5jlK72kNvJWDiqru9kVzcCt9mkLHIudk83g8yLS5FAURfKjhHAznHEen0yKkWyqvzBxcWFJH0yPUfw6pLP_xXdMmWYv-bZy2ppS9tS5YL7Xbqc0XO6Xxwwfb5x0Twyd76AnsQpwSYUfMei55p07zND8YATEAQi66GecYTe3HHBV4tjoHZQefrbKQJJVYmSymP8ZckI_q_n3Gm21AoX5YeK5j_tHEQ8zCnLkSXAJQB516jJEgvn-gRqX0xyKLCa04SLWUcdmCL0a8Dy2Wk7RP5janfzLRXmk0de271uORgPC0QYkzVFmEeVE02ZcOYeOurzuMpn0mVdynZyXoE3LUPMbaMQ5JFDivSV1lbpNWkufD474I7OKZe4_llAsmwyrDLHu10fuYCXcIvKkziPzpw3TrYtWGv4zh4Spo674H0C-98q90m_sHmsYsaoVvKWwo1y-fhFdtKCmY5Ycd_TUqAszjcW6DEfBkwUASER-lutlX7fc6Xc1rj8vRneOmzaWOi0dVe46OG-tBhtj-eB23ClkrkEzCCx7o2--yeLZ__sek7SZsEcpeMyi3bLyMbjbEpf7berK4yVx-FXPmYlN1664pIVbszQ9eTt4RjkZMzvAdq5ljNe13vRYzTPwIIFFfFBOz1O267DPlY3b9JrEG0FmC3osCChvi9eVIp9xe9SRTgsovFeOTrFvR9xJv1Sw0sOBdKekIH3_TSeb7oUpf9sLbXlAlgiO3-iMhLkltgHvAMpYkv2ZGdHDucMNsJ3m9GiNo96EiT8qHywROq9DMN0pgnQgU0QXWub4v09KuuT9fIBTg1sr4ApFJEntogZzhhvc7Zmz83n0zBEUjwNem6He-DnEyGO1JJXnAx2mfBMvNp3iP5V8Hxtdckrc6AtXIgW3NccL0mH-dG-3xodp10gvR7EblIevUVHA_qitToqrHNCfJ7LHU9UmlcmBmdvGLocBiQkHbH7ei_qok35T-BsMIXT5OIlUm30oRqvO2ZfdPoYtPDI-wD8WL5VAmNDdULzUTtjnZqX5AbKvf50huPHREiExOZg0YvNLyWfZtnVpDz9I5fIgliX2H_fvw1MDz5wlN6cT78I32F4odjFUkfebwEsbVK4Jl8uWmbySK_Ivr6qjH5IwhNeha-kS0zPXK5YnIbj54VVnETem2zJ4oNE-hbpJOfyev4xBFktZBnt-JzgCwplVsvLMVNqwM6JGH9espWp9degvKlDpQ5FrM5xVc1Ha-fKyJeoYKdz4vIk136TUoRSAuPHqyYAOrk=w1920-h673">
+            <h1 class="text-center text-white text-3xl font-bold">404 - Página Não Encontrada</h1>
+        </div>
+    </div>
+</template>
