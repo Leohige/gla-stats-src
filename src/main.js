8425fe0eef0b7ed69115261bb08e82c3ee44a040
Leandro Matheus <kesuhige@gmail.com>
2022-05-17 19:31:19 -0300

    Add routes

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import 'flowbite/dist/flowbite.js';
+import router from './routes'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
